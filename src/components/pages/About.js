5727d9d7c2f6ea41939f26ed6a8f972f373f5b45
dawit <dvwish9214@gmail.com>
2019-12-29 13:48:06 -0600

    finally update

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,8 +2,11 @@ import React from 'react';
 function About(){
  return (
 <React.Fragment>
+    <div className="about">
     <h1>About </h1>
     <p>It is to do list app .it is crash or refreshement course for ReactJS</p>
+    </div>
+    
 </React.Fragment>
  )   
 }
